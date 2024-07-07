b8ae5c9fc3cbf3c72b235301b3fb13f8f35ad797
jacobjea <jsw992@gmail.com>
2024-07-07 20:38:59 +0900

    none

---
+++ jacope/두수의합.java
@@ -0,0 +1,10 @@
+public class 두수의합 {
+    public static void main(String[] args) {
+        System.out.print( solution(4,9));
+    }
+
+    public static int solution(int num1, int num2){
+        int result = num1 * num2;
+        return result;
+    }
+}
