3e5600e3781302cd0b0e6de5c3e8fa84d0915bb0
jacobjea <jsw992@gmail.com>
2024-07-09 01:36:19 +0900

    풀이

--- jacope_programers/직사각형_별찍기.java
+++ jacope_programers/직사각형_별찍기.java
@@ -1,7 +1,10 @@
 import java.util.*;
+// Output size differs 오류로 콘솔에는 잘 찍히지만 프로그래머스에서 작동하지 않음...
+
 
 public class 직사각형_별찍기 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
@@ -13,16 +16,5 @@ public class 직사각형_별찍기 {
             }
         }
         System.out.print(builder.toString());
-    }
-
-    public static String solution(int n, int m) {
-        StringBuilder builder = new StringBuilder();
-        for (int i =0; i < m; i++){
-            builder.append("\n");
-            for (int j = 0; j < n; j++){
-                builder.append("*");
-            }
-        }
-        return builder.toString();
     }
 }
